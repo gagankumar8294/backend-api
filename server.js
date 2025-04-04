483f9c712c83df1b90783565208c81a68253e1f6
Gagan-Sky <gagankumar8294@gmail.com>
2025-04-04 06:33:00 +0530

    server file is ready

---
+++ server.js
@@ -0,0 +1,16 @@
+// import express
+import { express } from "express";
+
+// create server
+const server = express();
+
+// default request handler
+server.get('/', (req, res) => {
+    res.send('Backend Api - Project');
+})
+
+// listen on port 3200
+
+server.listen(3200 , () => {
+    console.log('listening on port 3200')
+})
